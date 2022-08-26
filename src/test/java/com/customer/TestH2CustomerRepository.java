e5895a3873eff78ae0f5d822f371ea16b3cde165
Anusha Joodala <anushaj2k10@gmail.com>
2022-08-26 15:50:52 -0500

    Code cleanup

--- src/test/java/com/customer/TestH2CustomerRepository.java
+++
@@ -1,8 +0,0 @@
-package com.customer;
-
-import com.customer.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestH2CustomerRepository extends JpaRepository<Customer,Integer> {
-
-}
--- src/test/java/com/customer/TestH2PurchaseHistoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.customer;
-
-import com.customer.entity.PurchaseHistory;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TestH2PurchaseHistoryRepository extends JpaRepository<PurchaseHistory,Integer> {
-
-}
